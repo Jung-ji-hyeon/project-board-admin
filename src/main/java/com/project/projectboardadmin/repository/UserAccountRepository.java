ed4e65371f3ed5253006feb1005cb6725720f2a1
Jung-ji-hyeon <ejxldortm@naver.com>
2024-02-16 21:10:27 +0900

    #6 - 회원 정보에 접근할 repository 인터페이스, jpa 설정 및 jp 테스트 작성
    회원 데이터에 접근할 리포지토리 인터페이스와
    jpa auditing 기능을 활성화시킬 `JpaConfig`,
    각자 다른 권한을 가진 테스트용 회원 데이터,
    crud 쿼리 생성을 확인할 jpa 테스트 작성

---
+++ src/main/java/com/project/projectboardadmin/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.project.projectboardadmin.repository;
+
+import com.project.projectboardadmin.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+}
