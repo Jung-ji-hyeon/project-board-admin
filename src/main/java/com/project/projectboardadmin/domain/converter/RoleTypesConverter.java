56df7edbd6461752c3b3c026d754f9454900e15d
Jung-ji-hyeon <ejxldortm@naver.com>
2024-02-20 19:01:50 +0900

    [리팩토링] thymeleaf 문법 적용 해결
    thymeleaf 문법이 스프링 부트 3 이상부터는 안되는 문법이 있으므로 스프링 부트 버전을 2.7로 변경

--- src/main/java/com/project/projectboardadmin/domain/converter/RoleTypesConverter.java
+++ src/main/java/com/project/projectboardadmin/domain/converter/RoleTypesConverter.java
@@ -1,9 +1,9 @@
 package com.project.projectboardadmin.domain.converter;
 
 import com.project.projectboardadmin.domain.constant.RoleType;
-import jakarta.persistence.AttributeConverter;
-import jakarta.persistence.Converter;
 
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
